f997cf02e2b92cdc11d9b3576ce422e656c69f0d
rogvold <sha-sabir@yandex.ru>
2012-10-25 20:57:35 +0400

    visualisation

--- BaseProjectCore/build/generated-sources/ap-source-output/ru/cardio/core/entity/Rate_.java
+++ BaseProjectCore/build/generated-sources/ap-source-output/ru/cardio/core/entity/Rate_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-10-21T20:02:37")
+@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-10-23T13:49:00")
 @StaticMetamodel(Rate.class)
 public class Rate_ { 
 
