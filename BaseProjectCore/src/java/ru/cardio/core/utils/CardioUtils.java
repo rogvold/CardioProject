de5e0e42470a0f458b637a7a4c6a8b1c172cf50a
rogvold <sha-sabir@yandex.ru>
2012-11-07 01:52:15 +0400

    for danon

--- BaseProjectCore/src/java/ru/cardio/core/utils/CardioUtils.java
+++ BaseProjectCore/src/java/ru/cardio/core/utils/CardioUtils.java
@@ -16,13 +16,13 @@ import java.util.regex.Pattern;
 public class CardioUtils {
     
     public static List<Integer> getIntervalsFromString(String text, String delimiter){
-        String ps = "([0-9]*"+delimiter+")*[0-9]+";
-        Pattern patt = Pattern.compile(ps);
-        Matcher matcher = patt.matcher(text);
-        if (!matcher.matches()){
-            System.out.println("incorrect data: " + text);
-            return null;
-        }
+//        String ps = "([0-9]*"+delimiter+")*[0-9]+";
+//        Pattern patt = Pattern.compile(ps);
+//        Matcher matcher = patt.matcher(text);
+//        if (!matcher.matches()){
+//            System.out.println("incorrect data: " + text);
+//            return null;
+//        }
         List<Integer> list = new ArrayList();
         String[] array = text.split(delimiter);
         for (String s: array){
