a7e5c8e2e952a9d05099370257f92fc5bf55081d
rogvold <sha-sabir@yandex.ru>
2012-11-09 00:53:59 +0400

    some cheats to prevent memory leak

--- BaseProjectCore/build/generated-sources/ap-source-output/ru/cardio/core/jpa/entity/User_.java
+++ BaseProjectCore/build/generated-sources/ap-source-output/ru/cardio/core/jpa/entity/User_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-11-06T20:11:54")
+@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-11-08T11:22:48")
 @StaticMetamodel(User.class)
 public class User_ { 
 
